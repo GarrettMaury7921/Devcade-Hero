67c99e552ddc35de8aa19db127713b05a0835104
Garrett Maury <90222973+GarrettMaury7921@users.noreply.github.com>
2023-02-05 01:33:10 -0500

    Menu Buttons Starter
    Implemented:
    - GameState.cs
    - State.cs
    - MenuState.cs
    - Button.cs
    
    Pictures now show up on the screen and are ready to be clicked. Some work needs to be done.
    - Need to:
    Document/Comment ALL files
    Credit Files
    Cleanup files
    Change from mouse to keyboard inputs

--- Game1.cs
+++ Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Devcade;
 using System.Diagnostics;
+using DevcadeGame.States;
 
 namespace DevcadeGame
 {
@@ -10,8 +11,18 @@ namespace DevcadeGame
 	{
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
-		
+
+		private State _currentState;
+		private State _nextState;
 		Texture2D main_menu;
+
+		// To see if sprite batch is currently drawing from other classes
+        public static bool isDrawing;
+
+        public void ChangeState(State state)
+		{
+			_nextState = state;
+		}
 
 		/// <summary>
 		/// Game constructor
@@ -20,7 +31,7 @@ namespace DevcadeGame
 		{
 			_graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
-			IsMouseVisible = false;
+			IsMouseVisible = true;
 		}
 
 		/// <summary>
@@ -30,20 +41,20 @@ namespace DevcadeGame
 		{
 			Input.Initialize(); // Sets up the input library
 
-			// Set window size if running debug (in release it will be full screen)
-			#region
+            // Set window size if running debug (in release it will be full screen)
+            #region
 #if DEBUG
-			_graphics.PreferredBackBufferWidth = 420;
-			_graphics.PreferredBackBufferHeight = 980;
-			_graphics.ApplyChanges();
+            //_graphics.PreferredBackBufferWidth = 420;
+            //_graphics.PreferredBackBufferHeight = 980;
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 980;
+            _graphics.ApplyChanges();
 #else
 			_graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
 			_graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
 			_graphics.ApplyChanges();
 #endif
 			#endregion
-			
-			// TODO: Add your initialization logic here
 
 			base.Initialize();
 		}
@@ -59,7 +70,8 @@ namespace DevcadeGame
 			// ex.
 			// texture = Content.Load<Texture2D>("fileNameWithoutExtention");
 
-			main_menu = Content.Load<Texture2D>("Menu_Assets/main_menu1");
+			main_menu = Content.Load<Texture2D>("Menu_Assets/vertical background");
+			_currentState = new MenuState(this, _graphics.GraphicsDevice, Content);
 		}
 
 		/// <summary>
@@ -80,7 +92,13 @@ namespace DevcadeGame
 				Exit();
 			}
 
-			// TODO: Add your update logic here
+			if (_nextState != null) 
+			{
+				_currentState = _nextState;
+				_nextState = null;
+			}
+			_currentState.Update(gameTime);
+			_currentState.PostUpdate(gameTime);
 
 			base.Update(gameTime);
 		}
@@ -95,13 +113,20 @@ namespace DevcadeGame
 
 			// Sprite Batch Begin
 			_spriteBatch.Begin();
-
+			isDrawing = true;
 			// Draw the main menu
-			_spriteBatch.Draw(main_menu, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight),
-				new Rectangle(0, 0, 1080, 2560), Color.White);
+			//_spriteBatch.Draw(main_menu, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight),
+			//	new Rectangle(0, 0, 1080, 2560), Color.White);
 
+			_currentState.Draw(gameTime, _spriteBatch);
+            foreach (var component in MenuState._components)
+            {
+                component.Draw(gameTime, _spriteBatch);
+            }
             _spriteBatch.End();
             // Sprite Batch End
+
+			isDrawing = false;
 
             base.Draw(gameTime);
 		}
---
+++ States/MenuState.cs
@@ -0,0 +1,101 @@
+﻿using DevcadeGame.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevcadeGame.States
+{
+    public class MenuState : State
+    {
+        public static List<Component> _components;
+
+        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            var buttonTexture = _content.Load<Texture2D>("Controls/hero");
+            var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
+
+            var newGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 200),
+                Text = "New Game",
+            };
+
+            newGameButton.Click += NewGameButton_Click;
+
+
+
+            var loadGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 250),
+                Text = "Load Game",
+            };
+
+            loadGameButton.Click += NewGameButton_Click;
+
+
+
+            var quitGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 300),
+                Text = "Quit",
+            };
+
+            quitGameButton.Click += NewGameButton_Click;
+
+            _components = new List<Component>()
+            {
+                newGameButton,
+                loadGameButton,
+                quitGameButton,
+            };
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // See if Game1 is drawing, if not, it is okay to draw the menu items
+            if (Game1.isDrawing != true)
+            {
+                Game1.isDrawing = true;
+                foreach (var component in _components)
+                {
+                    component.Draw(gameTime, spriteBatch);
+                }
+                Game1.isDrawing = false;
+            }
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            foreach (var component in _components)
+            {
+                component.Update(gameTime);
+            }
+        }
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            _game.ChangeState(new GameState(_game, _graphicsDevice, _content));
+        }
+
+        private void LoadGameButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void QuitGameButton_Click(object sender, EventArgs e)
+        {
+            _game.Exit();
+        }
+    }
+}
