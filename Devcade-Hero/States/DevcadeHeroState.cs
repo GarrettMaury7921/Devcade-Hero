734302877c4010bdca1ad69475cbe66430ac0650
Garrett Maury <jgmaury7921@gmail.com>
2023-03-21 12:27:33 -0400

    In Game Key Press

--- Devcade-Hero/States/DevcadeHeroState.cs
+++ Devcade-Hero/States/DevcadeHeroState.cs
@@ -5,6 +5,8 @@ using DevcadeGame.GameManager;
 using System.Collections.Generic;
 using System;
 using Microsoft.Xna.Framework.Media;
+using Devcade;
+using Microsoft.Xna.Framework.Input;
 
 namespace DevcadeGame.States
 {
@@ -25,6 +27,7 @@ namespace DevcadeGame.States
         private Texture2D background;
         private Song song;
         private String videoName;
+        private KeyboardState previousKeyboardState;
         private int count;
         private int highway_width;
         private int highway_height;
@@ -44,6 +47,23 @@ namespace DevcadeGame.States
         private int fred_lineX;
         private int fred_lineY;
         private int fred_line_offsetX;
+        // Keys
+        private Texture2D blue1down_pic;
+        private bool blue1down;
+        private Texture2D blue2down_pic;
+        private bool blue2down;
+        private Texture2D blue3down_pic;
+        private bool blue3down;
+        private Texture2D blue4down_pic;
+        private bool blue4down;
+        private Texture2D reddown_pic;
+        private bool reddown;
+        private Texture2D blue5down_pic;
+        private bool blue5down;
+        private Texture2D greendown_pic;
+        private bool greendown;
+        private Texture2D whitedown_pic;
+        private bool whitedown;
 
         public void Initialize()
         {
@@ -60,9 +80,20 @@ namespace DevcadeGame.States
             #endregion
 
             // Attributes                                                                       // COMMENTS BELOW
+            blue1down = false;
+            blue2down = false;
+            blue3down = false;
+            blue4down = false;
+            reddown = false;
+            blue5down = false;
+            greendown = false;
+            whitedown = false;
+
             highway_offset = 10;
             highwayX = (_preferredBackBufferWidth - highway_width) / 2;
             highwayY = _preferredBackBufferHeight - highway_height - highway_offset;
+
+            previousKeyboardState = Keyboard.GetState();
             
             // Fred Board parameters
             fred_board_offset = -55;                                                            // adjust as needed
@@ -81,6 +112,14 @@ namespace DevcadeGame.States
             highway = _content.Load<Texture2D>("Game_Assets/922Highway");
             fred_board = _content.Load<Texture2D>("Game_Assets/fred_board_gap");
             fred_line = _content.Load<Texture2D>("Game_Assets/note_line");
+            blue1down_pic = _content.Load<Texture2D>("Game_Assets/blue1down");
+            blue2down_pic = _content.Load<Texture2D>("Game_Assets/blue2down");
+            blue3down_pic = _content.Load<Texture2D>("Game_Assets/blue3down");
+            blue4down_pic = _content.Load<Texture2D>("Game_Assets/blue4down");
+            reddown_pic = _content.Load<Texture2D>("Game_Assets/reddown");
+            blue5down_pic = _content.Load<Texture2D>("Game_Assets/blue5down");
+            greendown_pic = _content.Load<Texture2D>("Game_Assets/greendown");
+            whitedown_pic = _content.Load<Texture2D>("Game_Assets/whitedown");
 
         } // Initialize Method
 
@@ -118,6 +157,55 @@ namespace DevcadeGame.States
             spriteBatch.Draw(highway, new Rectangle(highwayX, highwayY, highway_width, highway_height), Color.White);
 
             // Draw Fret Lines
+            DrawFredLines(spriteBatch);
+
+            // Draw the fred board
+            spriteBatch.Draw(fred_board, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
+
+            // Draw freds when pressed down
+            if (blue1down)
+            {
+                spriteBatch.Draw(blue1down_pic, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
+            }
+        } // Draw Method
+
+        // GAME CONTROLS:
+        
+        // KEYBOARD (Player 1):
+        //          TOP ROW: Q W E R
+        //       BOTTOM ROW: Z X C V
+        // GAME CONTROLS
+        // KEYBOARD (Player 2):
+        //          TOP ROW: 7 8 9 0
+        //       BOTTOM ROW: H J K L
+        public override void Update(GameTime gameTime)
+        {
+            // Make keyboard state
+            KeyboardState currentKeyboardState = Keyboard.GetState();
+
+            if (currentKeyboardState.IsKeyDown(Keys.Q) || Input.GetButton(1, Input.ArcadeButtons.A1)
+                     || Input.GetButton(2, Input.ArcadeButtons.A1))
+            {
+                blue1down = true;
+            }
+            else if (previousKeyboardState.IsKeyDown(Keys.Q) || Input.GetButtonDown(1, Input.ArcadeButtons.A1)
+             || Input.GetButtonDown(2, Input.ArcadeButtons.A1))
+            {
+                blue1down = false;
+            }
+
+            previousKeyboardState = currentKeyboardState;
+
+        } // Update method
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+
+        }
+
+        // OTHER METHODS
+        public void DrawFredLines(SpriteBatch spriteBatch)
+        {
             spriteBatch.Draw(fred_line, new Rectangle(fred_lineX, fred_lineY, fred_line_width, fred_line_height), Color.White);
             spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 34, fred_lineY, fred_line_width, fred_line_height), Color.White);
             spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 68, fred_lineY, fred_line_width, fred_line_height), Color.White);
@@ -126,19 +214,6 @@ namespace DevcadeGame.States
             spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 179, fred_lineY, fred_line_width, fred_line_height), Color.White);
             spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 213, fred_lineY, fred_line_width, fred_line_height), Color.White);
             spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 247, fred_lineY, fred_line_width, fred_line_height), Color.White);
-
-            // Draw the fred board
-            spriteBatch.Draw(fred_board, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-           
-        } // Update method
-
-        public override void PostUpdate(GameTime gameTime)
-        {
-
         }
 
     } // Devcade Hero state class
