3e1907614e40ab6f665938270b75356f99a0d8ac
Garrett Maury <jgmaury7921@gmail.com>
2023-03-21 17:42:58 -0400

    All keys on notes

--- Devcade-Hero/States/DevcadeHeroState.cs
+++ Devcade-Hero/States/DevcadeHeroState.cs
@@ -17,6 +17,7 @@ namespace DevcadeGame.States
         @ Draw Method
         @ Update Method
         @ PostUpdate Method
+        @ DrawFretLines Method
     */
     public class DevcadeHeroState : State
     {
@@ -29,24 +30,34 @@ namespace DevcadeGame.States
         private String videoName;
         private KeyboardState previousKeyboardState;
         private int count;
+
         private int highway_width;
         private int highway_height;
         private int highwayX;
         private int highwayY;
         private int highway_offset;
         private Texture2D highway;
+
         private Texture2D fred_board;
         private int fred_board_width;
         private int fred_board_height;
         private int fred_boardX;
         private int fred_boardY;
         private int fred_board_offset;
+
         private Texture2D fred_line;
         private int fred_line_width;
         private int fred_line_height;
         private int fred_lineX;
         private int fred_lineY;
         private int fred_line_offsetX;
+        private int fred_line2;
+        private int fred_line3;
+        private int fred_line4;
+        private int fred_line5;
+        private int fred_line6;
+        private int fred_line7;
+        private int fred_line8;
         // Keys
         private Texture2D blue1down_pic;
         private bool blue1down;
@@ -107,6 +118,13 @@ namespace DevcadeGame.States
             fred_line_height = highway_height + fred_board_offset + (fred_board_height - 5);    // Make the lines appear in the fred board
             fred_lineX = fred_boardX + fred_line_offsetX;                                       // apply offset to X
             fred_lineY = highwayY;
+            fred_line2 = fred_lineX + 34;
+            fred_line3 = fred_lineX + 68;
+            fred_line4 = fred_lineX + 103;
+            fred_line5 = fred_lineX + 145;
+            fred_line6 = fred_lineX + 179;
+            fred_line7 = fred_lineX + 213;
+            fred_line8 = fred_lineX + 247;
 
             // Load Assets
             highway = _content.Load<Texture2D>("Game_Assets/922Highway");
@@ -162,10 +180,39 @@ namespace DevcadeGame.States
             // Draw the fred board
             spriteBatch.Draw(fred_board, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
 
-            // Draw freds when pressed down
+            // Draw held freds when pressed down
             if (blue1down)
             {
                 spriteBatch.Draw(blue1down_pic, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
+            }
+            if (blue2down)
+            {
+                spriteBatch.Draw(blue2down_pic, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
+            }
+            if (blue3down)
+            {
+                spriteBatch.Draw(blue3down_pic, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
+            }
+            if (blue4down)
+            {
+                spriteBatch.Draw(blue4down_pic, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
+            }
+
+            if (reddown)
+            {
+                spriteBatch.Draw(reddown_pic, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
+            }
+            if (blue5down)
+            {
+                spriteBatch.Draw(blue5down_pic, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
+            }
+            if (greendown)
+            {
+                spriteBatch.Draw(greendown_pic, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
+            }
+            if (whitedown)
+            {
+                spriteBatch.Draw(whitedown_pic, new Rectangle(fred_boardX, fred_boardY, fred_board_width, fred_board_height), Color.White);
             }
         } // Draw Method
 
@@ -174,7 +221,7 @@ namespace DevcadeGame.States
         // KEYBOARD (Player 1):
         //          TOP ROW: Q W E R
         //       BOTTOM ROW: Z X C V
-        // GAME CONTROLS
+        //
         // KEYBOARD (Player 2):
         //          TOP ROW: 7 8 9 0
         //       BOTTOM ROW: H J K L
@@ -183,16 +230,7 @@ namespace DevcadeGame.States
             // Make keyboard state
             KeyboardState currentKeyboardState = Keyboard.GetState();
 
-            if (currentKeyboardState.IsKeyDown(Keys.Q) || Input.GetButton(1, Input.ArcadeButtons.A1)
-                     || Input.GetButton(2, Input.ArcadeButtons.A1))
-            {
-                blue1down = true;
-            }
-            else if (previousKeyboardState.IsKeyDown(Keys.Q) || Input.GetButtonDown(1, Input.ArcadeButtons.A1)
-             || Input.GetButtonDown(2, Input.ArcadeButtons.A1))
-            {
-                blue1down = false;
-            }
+            CheckP1Buttons(currentKeyboardState);
 
             previousKeyboardState = currentKeyboardState;
 
@@ -207,14 +245,89 @@ namespace DevcadeGame.States
         public void DrawFredLines(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(fred_line, new Rectangle(fred_lineX, fred_lineY, fred_line_width, fred_line_height), Color.White);
-            spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 34, fred_lineY, fred_line_width, fred_line_height), Color.White);
-            spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 68, fred_lineY, fred_line_width, fred_line_height), Color.White);
-            spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 103, fred_lineY, fred_line_width, fred_line_height), Color.White);
-            spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 145, fred_lineY, fred_line_width, fred_line_height), Color.White);
-            spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 179, fred_lineY, fred_line_width, fred_line_height), Color.White);
-            spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 213, fred_lineY, fred_line_width, fred_line_height), Color.White);
-            spriteBatch.Draw(fred_line, new Rectangle(fred_lineX + 247, fred_lineY, fred_line_width, fred_line_height), Color.White);
+            spriteBatch.Draw(fred_line, new Rectangle(fred_line2, fred_lineY, fred_line_width, fred_line_height), Color.White);
+            spriteBatch.Draw(fred_line, new Rectangle(fred_line3, fred_lineY, fred_line_width, fred_line_height), Color.White);
+            spriteBatch.Draw(fred_line, new Rectangle(fred_line4, fred_lineY, fred_line_width, fred_line_height), Color.White);
+            spriteBatch.Draw(fred_line, new Rectangle(fred_line5, fred_lineY, fred_line_width, fred_line_height), Color.White);
+            spriteBatch.Draw(fred_line, new Rectangle(fred_line6, fred_lineY, fred_line_width, fred_line_height), Color.White);
+            spriteBatch.Draw(fred_line, new Rectangle(fred_line7, fred_lineY, fred_line_width, fred_line_height), Color.White);
+            spriteBatch.Draw(fred_line, new Rectangle(fred_line8, fred_lineY, fred_line_width, fred_line_height), Color.White);
         }
+
+        public void CheckP1Buttons(KeyboardState currentKeyboardState)
+        {
+            if (currentKeyboardState.IsKeyDown(Keys.Z) || Input.GetButton(1, Input.ArcadeButtons.B1))
+            {
+                blue1down = true;
+            }
+            else if (previousKeyboardState.IsKeyDown(Keys.Z) || Input.GetButtonUp(1, Input.ArcadeButtons.B1))
+            {
+                blue1down = false;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.X) || Input.GetButton(1, Input.ArcadeButtons.B2))
+            {
+                blue2down = true;
+            }
+            else if (previousKeyboardState.IsKeyDown(Keys.X) || Input.GetButtonUp(1, Input.ArcadeButtons.B2))
+            {
+                blue2down = false;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.C) || Input.GetButton(1, Input.ArcadeButtons.B3))
+            {
+                blue3down = true;
+            }
+            else if (previousKeyboardState.IsKeyDown(Keys.C) || Input.GetButtonUp(1, Input.ArcadeButtons.B3))
+            {
+                blue3down = false;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.V) || Input.GetButton(1, Input.ArcadeButtons.B4))
+            {
+                blue4down = true;
+            }
+            else if (previousKeyboardState.IsKeyDown(Keys.V) || Input.GetButtonUp(1, Input.ArcadeButtons.B4))
+            {
+                blue4down = false;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.Q) || Input.GetButton(1, Input.ArcadeButtons.A1))
+            {
+                reddown = true;
+            }
+            else if (previousKeyboardState.IsKeyDown(Keys.Q) || Input.GetButtonUp(1, Input.ArcadeButtons.A1))
+            {
+                reddown = false;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.W) || Input.GetButton(1, Input.ArcadeButtons.A2))
+            {
+                blue5down = true;
+            }
+            else if (previousKeyboardState.IsKeyDown(Keys.W) || Input.GetButtonUp(1, Input.ArcadeButtons.A2))
+            {
+                blue5down = false;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.E) || Input.GetButton(1, Input.ArcadeButtons.A3))
+            {
+                greendown = true;
+            }
+            else if (previousKeyboardState.IsKeyDown(Keys.E) || Input.GetButtonUp(1, Input.ArcadeButtons.A3))
+            {
+                greendown = false;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.R) || Input.GetButton(1, Input.ArcadeButtons.A4))
+            {
+                whitedown = true;
+            }
+            else if (previousKeyboardState.IsKeyDown(Keys.R) || Input.GetButtonUp(1, Input.ArcadeButtons.A4))
+            {
+                whitedown = false;
+            }
+        } // CheckP1Buttons Method
 
     } // Devcade Hero state class
 
