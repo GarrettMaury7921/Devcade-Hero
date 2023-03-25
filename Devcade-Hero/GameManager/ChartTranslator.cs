5e91b0cefac3d0528122ce25aaf8853365e41aa4
Garrett Maury <jgmaury7921@gmail.com>
2023-03-25 17:55:03 -0400

    Chart Translator Done

--- Devcade-Hero/GameManager/ChartTranslator.cs
+++ Devcade-Hero/GameManager/ChartTranslator.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Devcade;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace DevcadeGame.GameManager
 {
@@ -12,12 +14,19 @@ namespace DevcadeGame.GameManager
         private int tsValue;
         private List<int> _bpms;
         private List<int> _bpm_time;
+        private List<int> _note_ticks;
+        private List<int> _note_color;
+        private List<int> _note_length;
 
         public ChartTranslator(List<String> notes)
         {
+            // Attributes
             _notes = notes;
             _bpms = new List<int>();
             _bpm_time = new List<int>();
+            _note_ticks = new List<int>();
+            _note_color = new List<int>();
+            _note_length = new List<int>();
 
             /*foreach (string _notes in notes)
             {
@@ -88,6 +97,72 @@ namespace DevcadeGame.GameManager
 
 
                 } // BPM if statement
+
+                /* Parse through the Events and get all of the regular note events
+                 *
+                 *   E - Global Events
+                 *   Text Events
+	             *       These events are simply a string to indicate something at a certain tick.
+	             *       They always contain 1 value, and that value is always surrounded by quotes “”. 
+	             *       Any quote characters within the event phrase will result in the event loading incorrectly, 
+	             *       depending on the program loading it. 
+                 *   Sections
+	             *       Sections are a subtype of text events and are commonly used by games in practice modes to 
+	             *       outline a certain section of notes to play over and over.
+	             *       It’s value is always prefixed with the string “section”, followed by the title of the section. 
+	             *       For example, a section labeled “Solo 1” would be stored as [112118 = E “section Solo 1”].
+                 *   Lyrics
+	             *       Similar to sections, lyrics are simply the same but it has the prefix “lyric” instead of “section”. 
+	             *       For example, a lyric would be stored as [112118 = E “lyric OOOoooo”]. 
+	             *       Lyrics events according to the Clone Hero spec need to start with a “phrase_start” event. 
+	             *       A “phase_end” event isn’t strictly required as a new “phase_start” event will automatically end the 
+	             *       previous phase.
+                 *
+                 */
+                else if (_notes.Contains("= N"))
+                {
+                    int firstValue = 0;
+                    int secondToLastValue = 0;
+                    int lastValue = 0;
+
+                    string[] parts = _notes.Split(' ');
+                    if (parts.Length >= 4)
+                    {
+                        int.TryParse(parts[2], out firstValue);
+                        int.TryParse(parts[parts.Length - 2], out secondToLastValue);
+                        int.TryParse(parts[parts.Length - 1], out lastValue);
+                    }
+
+                    // Add the values to the lists
+                    _note_ticks.Add(firstValue);
+                    _note_color.Add(secondToLastValue);
+                    _note_length.Add(lastValue);
+
+                    Debug.WriteLine(firstValue);
+                    Debug.WriteLine(secondToLastValue);
+                    Debug.WriteLine(lastValue);
+
+                    // DEBUGGING
+                    /*
+                     * Debug.WriteLine(firstValue);
+                    Debug.WriteLine(secondToLastValue);
+                    Debug.WriteLine(lastValue);
+                     * 
+                     * foreach (int num in _note_ticks)
+                    {
+                        Debug.WriteLine(num);
+                    }
+                    foreach (int num in _note_color)
+                    {
+                        Debug.WriteLine(num);
+                    }
+                    foreach (int num in _note_length)
+                    {
+                        Debug.WriteLine(num);
+                    }*/
+
+
+                } // Note if statement
 
             } // for each statement
 
